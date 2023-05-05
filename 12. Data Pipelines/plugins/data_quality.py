bc3535f326345203091e7edc22de7f1ab7b8188b
Quyen Le <52246911+quinlele@users.noreply.github.com>
2023-05-05 09:21:18 +0700

    Create data_quality.py

---
+++ 12. Data Pipelines/plugins/data_quality.py
@@ -0,0 +1,22 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class DataQualityOperator(BaseOperator):
+
+    ui_color = '#89DA59'
+
+    @apply_defaults
+    def __init__(self,
+                 conn_id = "",
+                 checks = [],
+                 *args, **kwargs):
+
+        super(DataQualityOperator, self).__init__(*args, **kwargs)
+
+        self.conn_id = conn_id
+        self.checks = checks
+        
+
+    def execute(self, context):
+        self.log.info('DataQualityOperator not implemented yet')
