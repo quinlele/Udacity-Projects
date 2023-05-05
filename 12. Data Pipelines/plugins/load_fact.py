2f4144513b3acc3c059025d4a961e987a6846881
Quyen Le <52246911+quinlele@users.noreply.github.com>
2023-05-05 09:21:42 +0700

    Create load_fact.py

---
+++ 12. Data Pipelines/plugins/load_fact.py
@@ -0,0 +1,23 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadFactOperator(BaseOperator):
+
+    ui_color = '#F98866'
+
+    @apply_defaults
+    def __init__(self,
+                 conn_id = "",
+                 checks = "",
+                 sql = "",
+                 *args, **kwargs):
+
+        super(LoadFactOperator, self).__init__(*args, **kwargs)
+
+        self.conn_id = conn_id
+        self.checks = checks
+        self.sql = sql
+
+    def execute(self, context):
+        self.log.info('LoadFactOperator not implemented yet')
