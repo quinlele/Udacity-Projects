20cb53ec95e606eb8976253741bf64c103556fd9
Quyen Le <52246911+quinlele@users.noreply.github.com>
2023-05-08 23:08:21 +0700

    Update stage_redshift.py

--- 12. Data Pipelines/plugins/operators/stage_redshift.py
+++ 12. Data Pipelines/plugins/operators/stage_redshift.py
@@ -13,6 +13,7 @@ class StageToRedshiftOperator(BaseOperator):
                  s3_bucket =. "",
                  s3_key = "",
                  region = "",
+                 table = "",
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
@@ -25,7 +26,20 @@ class StageToRedshiftOperator(BaseOperator):
         self.region = region
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
+        self.log.info('StageToRedshiftOperator start')
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
+        hook = PostgresHook(self.conn_id)
+
+        format_sql = StageToRedshiftOperator.copy_sql.format(
+            self.table, s3_path, credentials.access_key,
+            credentials.secret_key, self.region)
+   
+        
+
+        hook.run(format_sql)
+        self.log.info(
+            f"StageToRedshiftOperator finish copy - {self.table}")
 
 
 
